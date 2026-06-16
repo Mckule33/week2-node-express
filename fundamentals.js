e2f70096501365d12fa0aaff1e8ceb521c94c223
Mawulawoe Kodjo Kulekpo <mawulawoekulekpo@gmail.com>
2026-06-16 09:32:23 +0000

    Initial commit: Fundamentals to know

---
+++ fundamentals.js
@@ -0,0 +1,22 @@
+// "CONST" for values that dont change 
+const name = "mawulawoe"
+
+console.log(name)// runs an output to the console(whats in the bracket)
+
+// for values that change along the line 
+let age = 30;
+age = 25;
+
+
+const name = "mawulawoe" //string
+const age = 25; //number
+const isActive = true; //boolean
+const fruits = ["mango", "banana"]; //array
+const user = { name:  "mckule", age: 25} //object
+
+// Functions
+function greet(name){
+    return 'hello' + ' ' + name;
+}
+console.log(greet("Mawulawoe"));
+
